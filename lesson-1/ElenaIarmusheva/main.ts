cb96d041c366248f42b19de166d7ed2548caced7
ellnka <alenkasmail@gmail.com>
2017-01-31 20:49:34 +0200

    Update main.ts

--- lesson-1/ElenaIarmusheva/main.ts
+++ lesson-1/ElenaIarmusheva/main.ts
@@ -1,4 +1,4 @@
-type menuList = {title: string; items: menuList}[];
+type menuList = {title: string; items?: menuList}[];
 
 const menuList: menuList = [
   {
@@ -53,7 +53,7 @@ function generateMenu(list: menuList): string {
 let navMenuList = document.querySelector('.menu') as HTMLDivElement;
 navMenuList.innerHTML = generateMenu(menuList);
 navMenuList.onclick = (ev: MouseEvent) => {
-  let el = <HTMLAnchorElement>ev.target;
+  let el = ev.target as HTMLAnchorElement;
   let classList = el.classList;
   if (!classList.contains('title')) {
     return;
