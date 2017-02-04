fd2f204baec135b7a63cae354947e264d7aa23b3
LinkFly <linkfly@yandex.ru>
2017-02-04 22:23:03 +0300

    sergey-katrevich. lesson2 hometask - slider - wrap into class (+ tslint allow for 2 class per file)

--- lesson-2/SergeyKatrevich/slider/slider.ts
+++ lesson-2/SergeyKatrevich/slider/slider.ts
@@ -4,56 +4,109 @@
 /*6) Реализуйте слайдер
 http://learn.javascript.ru/task/slider
 */
+
+////////////////////// SliderBase ///////////////////////////
+abstract class SliderBase {
+  private moveAble: boolean = false;
+  public constructor(protected elContainer: HTMLDivElement,
+                     protected elBox: HTMLDivElement,
+                     protected elSlide: HTMLDivElement) {
+    this.addListeners();
+    this.setSliderXByMouseX(200);
+  }
+
+  public addListeners(): void {
+    this.elSlide.addEventListener('mousedown', () => { this.moveAble = true; });
+
+    window.addEventListener('mouseup', () => {
+      if (this.moveAble) { this.moveAble = false; }
+    });
+
+    this.elContainer.addEventListener('mousemove', this.mouseMoveHandler.bind(this));
+  }
+
+  protected getSliderSlideW(): number {
+    let pxWidth: string = getComputedStyle(this.elSlide).width;
+    pxWidth = pxWidth.slice(0, -2); // delete px at the end
+    return parseInt(pxWidth, 10);
+  }
+
+  protected mouseMoveHandler(ev: MouseEvent): void {
+    if (!this.moveAble) { return; }
+    this.setSliderXByMouseX(ev.clientX);
+  };
+
+  protected setSliderXByMouseX(mouseX: number): void {
+    let curSlideX: number = mouseX - this.getSliderSlideW() / 2;
+    curSlideX = this.correctSlideX(curSlideX);
+    elSliderSlide.style.left = `${curSlideX}px`;
+  }
+
+  protected abstract correctSlideX(x: number): number;
+}
+
+////////////////////// Slider ///////////////////////////
+interface ISliderState {
+  x: number;
+}
+interface ISliderParts {
+  container: HTMLElement;
+  box: HTMLElement;
+  slide: HTMLElement;
+}
+interface ISlider {
+  state: ISliderState;
+  getParts(): ISliderParts;
+  report?(): void;
+}
+
+class Slider extends SliderBase implements ISlider {
+  private x: number;
+
+  public get state(): ISliderState {
+    return { x: this.x };
+  }
+  public getParts(): ISliderParts {
+    return {
+      box: this.elBox,
+      container: this.elContainer,
+      slide: this.elSlide,
+    };
+  }
+
+  public report() {
+    const parts: ISliderParts = this.getParts();
+    const x: number = this.state.x;
+    console.log(`Slider details:
+x = ${x}
+container = ${parts.container}
+box = ${parts.box}
+slide = ${parts.slide}
+  `);
+  }
+
+  protected correctSlideX(x: number): number {
+    const sliderSlideW = this.getSliderSlideW();
+    const rect: ClientRect = this.elBox.getBoundingClientRect();
+    const sliderMinX = rect.left;
+    const sliderMaxX = rect.right;
+    x -= sliderMinX;
+    const maxX: number = sliderMaxX - sliderMinX - sliderSlideW;
+    if (x < 0) {
+      x = 0;
+    } else if (x > maxX) {
+      x = maxX;
+    }
+    this.x = x;
+    return x;
+  }
+}
+
+///////////////////////// Using Slider /////////////////////////
 let sel: Function = document.querySelector.bind(document);
 let elSliderContainer: HTMLDivElement = sel('.slider-container');
 let elSliderSlide: HTMLDivElement = sel('.slider-slide');
 let elSliderBox: HTMLDivElement = sel('.slider-box');
-let moveAble: boolean = false;
-let sliderSlideW: number;
-let sliderMinX: number;
-let sliderMaxX: number;
-
-function getSliderSlideW(el: HTMLDivElement): number {
-  let pxWidth: string = getComputedStyle(el).width;
-  pxWidth = pxWidth.slice(0, -2); // delete px at the end
-  return parseInt(pxWidth, 10);
-}
-
-function correctSlideX(x: number): number {
-  x -= sliderMinX;
-  const maxX: number = sliderMaxX - sliderMinX - sliderSlideW;
-  if (x < 0) {
-    x = 0;
-  } else if (x > maxX) {
-    x = maxX;
-  }
-  console.log('corrected: ', x);
-  return x;
-}
-
-function mouseMoveHandler(this: void, ev: MouseEvent): void {
-  if (!moveAble) { return; }
-  let curSlideX: number = ev.clientX - sliderSlideW / 2;
-  curSlideX = correctSlideX(curSlideX);
-  elSliderSlide.style.left = `${curSlideX}px`;
-}
-
-function addListeners(): void {
-  elSliderSlide.addEventListener('mousedown', () => { moveAble = true; });
-
-  window.addEventListener('mouseup', () => {
-    if (moveAble) { moveAble = false; }
-  });
-
-  elSliderContainer.addEventListener('mousemove', mouseMoveHandler);
-}
-
-function sliderInit(): void {
-  sliderSlideW = getSliderSlideW(elSliderSlide);
-  const rect: ClientRect = elSliderBox.getBoundingClientRect();
-  sliderMinX = rect.left;
-  sliderMaxX = rect.right;
-  addListeners();
-}
-
-sliderInit();
+const slider = new Slider(elSliderContainer, elSliderBox, elSliderSlide);
+console.log('slider: ', slider, 'state: ', slider.state, 'parts: ', slider.getParts());
+slider.report();
